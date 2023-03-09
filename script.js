6ca1ed8fd814b6e41c4459b82ed045a6bbc93643
ZeYohArt <126535526+ZeYohArt@users.noreply.github.com>
2023-03-10 00:24:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
-function sum(){
-    var n1 = (parseFloat(document.getElementById("n1").value));
-    var n2 = (parseFloat(document.getElementById("n2").value));
+function sum(n1, n2){
     var sum = n1 + n2;
     document.getElementById("footer").innerHTML = "The sum of "+ n1 +" and "+ n2 +" is "+ sum +".";
 }
 
-function difference(){
-    var n1 = (parseFloat(document.getElementById("n1").value));
-    var n2 = (parseFloat(document.getElementById("n2").value));
+function difference(n1, n2){
     var difference = n1 - n2;
     document.getElementById("footer").innerHTML = "The difference of "+ n1 +" and "+ n2 +" is "+ difference +".";
 }
 
-function product(){
-    var n1 = (parseFloat(document.getElementById("n1").value));
-    var n2 = (parseFloat(document.getElementById("n2").value));
+function product(n1, n2){
     var product = n1 * n2;
     document.getElementById("footer").innerHTML = "The product of "+ n1 +" and "+ n2 +" is "+ product +".";
 }
 
-function quotient(){
-    var n1 = (parseFloat(document.getElementById("n1").value));
-    var n2 = (parseFloat(document.getElementById("n2").value));
+function quotient(n1, n2){
     var quotient = n1 / n2;
     document.getElementById("footer").innerHTML = "The quotient of "+ n1 +" and "+ n2 +" is "+ quotient +".";
 }
 
-function remainder(){
-    var n1 = (parseFloat(document.getElementById("n1").value));
-    var n2 = (parseFloat(document.getElementById("n2").value));
+function remainder(n1, n2){
     var remainder = n1 % n2;
     document.getElementById("footer").innerHTML = "The remainder of "+ n1 +" divided by "+ n2 +" is "+ remainder +".";
 }
